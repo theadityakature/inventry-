7023332e2ac2a4239cb5877a08d0ba4ed0562eb2
adityakature <theadityakature@gmail.com>
2025-09-14 21:07:17 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Dashbord } from './pages/dashbord/dashbord';
+// import { DashboardComponent } from './dashboard/dashboard.component';
+// import { ShopComponent } from './shop/shop.component';
+// import { PurchaseComponent } from './purchase/purchase.component';
+// import { ProductsComponent } from './products/products.component';
+// import { SettingsComponent } from './settings/settings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'dashboard', component: Dashbord},
+  { path: 'shop', component: Dashbord },
+  { path: 'purchase', component: Dashbord },
+  { path: 'products', component: Dashbord },
+  { path: 'settings', component: Dashbord },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' } // default route
+];
---
+++ src/app/pages/dashbord/dashbord.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Navbar } from '../../navbar/navbar';
+
+@Component({
+  selector: 'app-dashbord',
+  imports: [Navbar],
+  templateUrl: './dashbord.html',
+  styleUrl: './dashbord.css'
+})
+export class Dashbord {
+
+}
